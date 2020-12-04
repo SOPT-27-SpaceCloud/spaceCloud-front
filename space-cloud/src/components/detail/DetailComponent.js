05164d20e78c0ff0657601378fb3e26690f9c1bc
Say-young <seyoung990525@gmail.com>
2020-12-05 03:48:01 +0900

    [add] 하단 버튼 추가

--- space-cloud/src/components/detail/DetailComponent.js
+++ space-cloud/src/components/detail/DetailComponent.js
@@ -4,6 +4,7 @@ import listAPI from '../../lib/listAPI'
 import DetailMiddleComponent from './DetailMiddleComponent'
 import DetailTopComponent from './DetailTopComponent'
 import Slider from "./Slider";
+
 const DetailComponent = () => {
 
     //나중에 서버 연결 후 정보 받아오는 것 구현
@@ -14,6 +15,7 @@ const DetailComponent = () => {
             <DetailTopComponent/>
             <DetailMiddleComponent/>
         </div>
+        
     );
 
 };
